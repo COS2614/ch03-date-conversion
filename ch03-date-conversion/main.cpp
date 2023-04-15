67a559c638481b180e4d3546ad410ac59355066f
mitchcamza <192mitch88@gmail.com>
2023-04-15 16:25:30 +0200

    Create a QDate object and display the date using QTextStream

--- ch03-date-conversion/main.cpp
+++ ch03-date-conversion/main.cpp
@@ -1,11 +1,25 @@
-#include "widget.h"
+#include <QCoreApplication>
+#include <QDate>
+#include <QTextStream>
 
-#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return a.exec();
+    QCoreApplication a(argc, argv);
+
+    // Create a QDate object for today's date
+    QDate currentDate = QDate::currentDate();
+
+    // Display the date in various formats using QTextStream
+    QTextStream out(stdout);
+
+    out << "Today's date: " << currentDate.toString() << Qt::endl;
+    out << "Year: " << currentDate.year() << Qt::endl;
+    out << "Month (number): " << currentDate.month() << Qt::endl;
+    out << "Month (name): " << currentDate.toString("MMMM") << Qt::endl;
+    out << "Day of week (number): " << currentDate.dayOfWeek() << Qt::endl;
+    out << "Day of week (name): " << currentDate.toString("dddd") << Qt::endl;
+    out << "Day of year: " << currentDate.dayOfYear() << Qt::endl;
+
+    return 0;
 }
